1a43943d511f1881c3a055ae424e413bdfe0658c
MSI <aleksander.inglot98@gmail.com>
2023-01-25 17:09:35 +0100

    feature/updated QueryRunner.java

--- src/main/java/com/olson1998/cassandra/adapter/DefaultNextQuery.java
+++ src/main/java/com/olson1998/cassandra/adapter/DefaultNextQuery.java
@@ -4,6 +4,7 @@ import com.datastax.driver.core.Session;
 import com.datastax.driver.mapping.Mapper;
 
 public class DefaultNextQuery<A, D> extends NextQuery<A, D>{
+
     protected DefaultNextQuery(A argument, Session session, Mapper<D> defaultMapper) {
         super(argument, session, defaultMapper);
     }
